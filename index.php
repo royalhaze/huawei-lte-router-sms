6aaaebcb7dd30616a0ad34f33594068741e3fa79
ALI <42044226+royalhaze@users.noreply.github.com>
2025-01-09 19:52:47 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,91 @@
 <?php
+
 /**
  * GitHub: RoyalHaze
  * Date: 10/4/23
  * Time: 12:56 AM
  **/
+
 require 'vendor/autoload.php';
 
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 
+class RouterSmsAutomation
+{
+    private $driver;
+    private $routerIp;
+    private $password;
+    private $text;
+    private $number;
+
+    public function __construct($routerIp, $password, $text, $number)
+    {
+        $this->routerIp = $routerIp;
+        $this->password = $password;
+        $this->text = $text;
+        $this->number = $number;
+
+        // Initialize WebDriver
+        $this->driver = RemoteWebDriver::create('http://127.0.0.1:4444', [
+            'platform' => 'ANY',
+            'browserName' => 'chrome',
+        ]);
+    }
+
+    public function sendSms()
+    {
+        try {
+            // Open the router login page
+            $this->driver->get("http://{$this->routerIp}");
+
+            // Wait for the page to load
+            $this->driver->wait(10)->until(
+                WebDriverExpectedCondition::titleContains('4G')
+            );
+
+            // Login to the router
+            $this->driver->findElement(WebDriverBy::id('login_password'))->sendKeys($this->password);
+            $this->driver->findElement(WebDriverBy::id('login_btn'))->click();
+
+            // Wait for the tools menu to load
+            $this->driver->wait(10)->until(
+                WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id('menu_tools'))
+            );
+
+            // Navigate to SMS section
+            $this->driver->findElement(WebDriverBy::id('menu_tools'))->click();
+            $this->driver->wait(10)->until(
+                WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id('sms_message_new'))
+            );
+
+            // Send SMS
+            $this->driver->findElement(WebDriverBy::id('sms_message_new'))->click();
+            $this->driver->findElement(WebDriverBy::id('sms_send_user_input'))->sendKeys($this->number);
+            $this->driver->findElement(WebDriverBy::id('sms_current_content'))->sendKeys($this->text);
+            $this->driver->findElement(WebDriverBy::cssSelector('div[class="sms_send_normal"][onclick*="EMUI.smsSendAndSaveController.sendMessage()"]'))->click();
+
+            // Wait for the SMS to be sent
+            $this->driver->wait(30)->until(
+                WebDriverExpectedCondition::invisibilityOfElementLocated(WebDriverBy::id('submit_light'))
+            );
+
+            echo "SMS sent successfully.\n";
+        } catch (Exception $e) {
+            echo "An error occurred: " . $e->getMessage() . "\n";
+        } finally {
+            // Ensure the WebDriver quits
+            $this->driver->quit();
+        }
+    }
+}
+
+// Usage
+$routerIp = '192.168.1.1';
+$password = 'password';
 $text = 'TEXT';
 $number = 'NUM';
-$password = 'password';
-$router_ip = '192.168.1.1';
 
-
-$driver = \Facebook\WebDriver\Remote\RemoteWebDriver::create('http://127.0.0.1:4444', [
-    'platform' => 'ANY',
-    'browserName' => 'chrome',
-]);
-
-$driver->get("http://$router_ip");
-
-$driver->wait(10)->until(
-    \Facebook\WebDriver\WebDriverExpectedCondition::titleContains('4G')
-);
-
-$driver->findElement(\Facebook\WebDriver\WebDriverBy::id('login_password'))->sendKeys($password);
-
-$driver->findElement(\Facebook\WebDriver\WebDriverBy::id('login_btn'))->click();
-
-
-$driver->wait(10)->until(
-    \Facebook\WebDriver\WebDriverExpectedCondition::presenceOfElementLocated(\Facebook\WebDriver\WebDriverBy::id('menu_tools'))
-);
-
-
-$driver->findElement(\Facebook\WebDriver\WebDriverBy::id('menu_tools'))->click();
-
-
-$driver->wait(10)->until(
-    \Facebook\WebDriver\WebDriverExpectedCondition::presenceOfElementLocated(\Facebook\WebDriver\WebDriverBy::id('sms_message_new'))
-);
-
-$driver->findElement(\Facebook\WebDriver\WebDriverBy::id('sms_message_new'))->click();
-
-$driver->findElement(\Facebook\WebDriver\WebDriverBy::id('sms_send_user_input'))->sendKeys($number);
-
-$driver->findElement(\Facebook\WebDriver\WebDriverBy::id('sms_current_content'))->sendKeys($text);
-
-$driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector('div[class="sms_send_normal"][onclick*="EMUI.smsSendAndSaveController.sendMessage()"]'))->click();
-
-$driver->wait(30)->until(
-    \Facebook\WebDriver\WebDriverExpectedCondition::invisibilityOfElementLocated(\Facebook\WebDriver\WebDriverBy::id('submit_light'))
-);
-
-$driver->quit();
+$automation = new RouterSmsAutomation($routerIp, $password, $text, $number);
+$automation->sendSms();
